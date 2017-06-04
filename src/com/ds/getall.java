338aa0af14bf77139a0de9518098e137b71476ce
dhansekar <mail2dhanasekar13@gmail.com>
2017-06-05 00:45:56 +0530

    pass url to view all json object

--- src/com/ds/getall.java
+++ src/com/ds/getall.java
@@ -2,11 +2,16 @@ package com.ds;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+//import com.google.appengine.repackaged.org.codehaus.jackson.map.ObjectMapper;
 
 public class getall extends HttpServlet{
 	/**
@@ -16,16 +21,30 @@ public class getall extends HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("application/json");
+		ObjectMapper map=new ObjectMapper();
+		HashSet<Object> hm=new HashSet<Object>();
 		PrintWriter out1=resp.getWriter();
-		System.out.println("this is the first");
-		javax.jdo.Query q1=register.data.newQuery(seconddb.class);
-		System.out.println("this is the second");
-		@SuppressWarnings("unchecked")
+			javax.jdo.Query q1=register.data.newQuery(seconddb.class);
+			@SuppressWarnings("unchecked")
 		List<seconddb> listdata=(List<seconddb>)q1.execute();
-		/*if(!listdata.isEmpty()){
-		out1.println(listdata);
-		}*/
-		out1.println(listdata);
+	int i=0;
+		for(seconddb ki:listdata)
+		{	hm.add(map.writeValueAsString(ki));
+		/*System.out.println(i+"   --  This is inside the for loop   -  "+ki);
+		System.out.println("for name  --  "+ki.getn());
+		System.out.println("for blog  -- "+ki.getb());   
+		System.out.println("for previous comment -- "+ki.getpre());
+		System.out.println("for new commment --  "+ki.getnew());*/
+			/*hm.put(i, ki.getn());
+			hm.put(i, ki.getb());
+			hm.put(i, ki.getpre());
+			hm.put(i, ki.getnew());	*/
+		i++;
+		
+		}
+	//	map.writeValue(seconddb.class, hm);
+		
+		out1.println(hm);
 	
 	}
 }
--- war/script.js
+++ war/script.js
@@ -8,15 +8,48 @@ $(function(){
 		}	
 		
 	});*/
-	console.log("this is in script.js")
+	
 	var xh=new XMLHttpRequest();
 	xh.onreadystatechange=function(){
 		if(this.readyState==4&&this.status==200)
 		{
-			$("#main").append(xh.responseText);
+			g(xh.responseText);
+			//$("#main").append(xh.responseText);
 		}
 	};
 xh.open("GET","g1",true);
 xh.send();
 	
 });
+
+function g(o)
+{
+	//have to take the h to json value to make the individual 
+var gh=$("#h").val();
+if(gh==1)
+	{console.log("this is the first block"+o);
+	 obj=JSON.parse(o);
+	 for (i=0;i<obj.length;i++){//	console.log(obj[i].b);
+	/*$("#a").append(obj[i].n);
+	$("#b").append(obj[i].b);
+	$("#c").append(obj[i].pre);
+	$("#d").append(obj[i].new);*/
+		 $("#main").append(obj[i].b);
+	 
+	 }
+	/*var i=new Object();
+	 i=JSON.parse(h);
+console.log(i);*/
+	
+	}
+if(gh==2)
+	{console.log("this is the second block"+o);
+	
+	/*var i=JSON.parse(h);
+	console.log(i);*/
+
+	 obj=JSON.parse(o);
+	console.log();
+	}
+
+}
